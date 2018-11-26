b7334baf57ac512b756417a122ec5cd24cec7a50
Pavlos Karakalidis <pavlosk@mkinet.net>
2018-11-26 11:38:17 +0200

    Changed the position of the bot, the size of carousel and fixed an issue with css files

--- src/main/WebApp/index.js
+++ src/main/WebApp/index.js
@@ -4,5 +4,6 @@ $(function () {
     $('head').append('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">');
     $('head').append('<script src = "https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js" ></script >');
     $('head').append('<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>');
+    $('link[href="/menus/faqpage.css"]').prop('disabled', true);
     $("#webpage").load("carousel.html");
 });
