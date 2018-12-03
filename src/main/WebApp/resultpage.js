3b610ff57f984454dea1bf27dbd88fb8dfb13bcf
pavlkara1 <pavloc.kara@outlook.com>
2018-12-03 11:02:04 +0200

    Now the user can search based on the price range

--- src/main/WebApp/resultpage.js
+++ src/main/WebApp/resultpage.js
@@ -1,7 +1,15 @@
-$(document).ready(function() {
+$(document).ready(function sendRequest() {
     $.ajax({
-    url: "/getAllHouses"
- }).then(function(data) {
+    url: "/rea/building/query/pricerange",
+        data: {"min":$(".priceMin").val(),
+                "max":$(".priceMax").val()}
+ }).then(function getData(data) {
+     if(data.length===0){
+         var block0= document.createElement("div");
+            block0.append("No property was found based on your criteria");
+            block0.style.cssText="vertical-align: top;padding-left: 5%;padding-top:3%;font-size:18px;border:none;"
+            $('.coveo-results-column').append(block0);
+     }
     $.each(data, function (key, entry) {
         var block0 = document.createElement("div");
         block0.className="coveo-list-layout"
@@ -10,26 +18,26 @@ $(document).ready(function() {
         block5.className="coveo-result-frame";
         var block = document.createElement("div");
         block.className="coveo-result-cell";
-        block.style.cssText="vertical-align: top;text-align:center;width:32px";
+        block.style.cssText="vertical-align: top;text-align:center;width:32px;border:none";
         var block1 =document.createElement("div");
         block1.className="coveo-result-row";
-        block.style.cssText="vertical-align: top;padding-left: 16px;";
+        block.style.cssText="vertical-align: top;padding-left: 16px;border:none;";
         var block2 = document.createElement("div");
         block2.className="coveo-result-row";
-        block2.style.cssText="margin-top:0";
+        block2.style.cssText="margin-top:0;border:none";
         var block3 = document.createElement("div");
         block3.className="coveo-result-cell";
-        block3.style.cssText="vertical-align:top;font-size:16px;";
+        block3.style.cssText="vertical-align:top;font-size:16px;border:none";
         var block4 = document.createElement("pre");
         block4.append(entry.type.name+" "+ entry.id);
         var price = document.createElement("pre");
         price.append(entry.price+ "€");
-        price.style.cssText="text-align:right;margin-right:3%;";
+        price.style.cssText="text-align:right;margin-right:3%;border:none";
         block4.append(price);
-        block4.style.cssText = "margin-left:2%";
+        block4.style.cssText = "margin-left:2%;border:none";
         var description = document.createElement("div");
         description.className="coveo-result-row";
-        description.style.css="margin-top:10px;";
+        description.style.css="margin-top:10px;border:none";
         var description1 = document.createElement("div");
         description1.className="coveo-result-cell";
         var description2 = document.createElement("span");
